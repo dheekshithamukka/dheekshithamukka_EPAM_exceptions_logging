6c5e055e3324177f79617e8f09a8e34a0d1ba5db
Mukka Dheekshitha <41354527+dheekshithamukka@users.noreply.github.com>
2020-07-24 20:00:26 +0530

    Add files via upload

---
+++ EPAMCleanCodeHC/src/main/java/epam/com/cc/ConstructionCost.java
@@ -0,0 +1,20 @@
+package epam.com.cc;
+
+public class ConstructionCost {
+
+	public int stdmat(int area) {
+		return area*1200;
+	}
+
+	public int abstdmat(int area) {
+		return area*1500;
+	}
+
+	public int hstdmat(int area) {
+		return area*1800;
+	}
+
+	public int fullaut(int area) {
+		return area*2500;
+	}
+}
---
+++ EPAMSimpleInterest/src/main/java/EPAM/EPAMSimpleInterest/SimpleInterest.java
@@ -0,0 +1,8 @@
+package EPAM.EPAMSimpleInterest;
+
+public class SimpleInterest {
+	public static double simpleinterest(double principle_amount, double time, double rate) {
+		double si = (principle_amount * time * rate)/100;
+		return si;
+	}
+}
